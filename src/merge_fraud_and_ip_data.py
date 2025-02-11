c349bf596a90d291db3d0efef667cb732a4eb84c
yonas bogale <pronivamusic14@gmail.com>
2025-02-11 19:40:27 +0300

    Add script to merge fraud data with IP datasets for enriched analysis

--- src/merge_fraud_and_ip_data.py
+++ src/merge_fraud_and_ip_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 fraud_data = pd.read_csv('../data/Fraud_Data.csv')
 ip_data = pd.read_csv('../data/IpAddress_to_Country.csv')
-
+ 
 merged_df = pd.concat([fraud_data, ip_data], axis = 1)
 merged_df = merged_df.dropna()
 print(merged_df.columns)
